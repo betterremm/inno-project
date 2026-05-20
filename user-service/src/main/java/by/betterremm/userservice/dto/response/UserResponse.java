3d85c081492242c2d0386b3dc6d156968e677299
Mikhail <doabledguy@gmail.com>
2026-05-21 02:23:55 +0300

    ServiceLayer

---
+++ user-service/src/main/java/by/betterremm/userservice/dto/response/UserResponse.java
@@ -0,0 +1,16 @@
+package by.betterremm.userservice.dto.response;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record UserResponse(
+        Long id,
+        String name,
+        String surname,
+        LocalDate birthDate,
+        String email,
+        boolean active,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+}
---
+++ user-service/src/main/java/by/betterremm/userservice/dto/response/UserWithCardsResponse.java
@@ -0,0 +1,18 @@
+package by.betterremm.userservice.dto.response;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record UserWithCardsResponse(
+        Long id,
+        String name,
+        String surname,
+        LocalDate birthDate,
+        String email,
+        boolean active,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        List<PaymentCardResponse> cards
+) {
+}
